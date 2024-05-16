44a432ecc65f6125b6af25c161cd46a8cae39bae
shweta.borganve <shweta.borganve@gmail.com>
2024-05-16 13:19:53 +0000

    Adding natural numbers

---
+++ py_basics/add_numbers.py
@@ -0,0 +1,12 @@
+# Sum of natural numbers up to num
+num = 20
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
