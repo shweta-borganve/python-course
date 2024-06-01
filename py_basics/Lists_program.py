26839df6ec19b4e535c523ef1d81ca71927f68ff
shweta.borganve <shweta.borganve@gmail.com>
2024-06-01 06:57:46 +0000

    tupules python program

---
+++ py_basics/Lists_program.py
@@ -0,0 +1,7 @@
+empty_list = []
+numbers = [2,4,6,8,10]
+cars = ['maruti', 'hyundai', 'honda']
+
+print(type(empty_list))
+print(type(numbers))
+print(type(cars))
---
+++ py_basics/Tupules_program.py
@@ -0,0 +1,5 @@
+name_of_tuple = ('apple', 10, 'banana', 24)
+print(name_of_tuple[0])
+print(name_of_tuple[1])
+print(name_of_tuple[2])
+print(name_of_tuple[3])
