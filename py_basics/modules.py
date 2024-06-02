fa041b1fa7eeabf876f290d002a2a109b3c6b0f4
shweta.borganve <shweta.borganve@gmail.com>
2024-06-02 17:45:15 +0000

    modules.py

--- py_basics/modules.py
+++ py_basics/modules.py
@@ -1,9 +1,13 @@
 def greet(name, language):
     if language == "english":
-        greet = "nice to meet you"
+        greet = "nice to meet you: "
     elif language == "klingon":
-        greet = "hghhg"
+        greet = "hghhg: "
     elif language == "elvish":
-        greet = "gi suilon"
+        greet = "gi suilon : "
     return greet + name
-    greet('ben', 'english')
+    
+print(greet('ben', 'english'))
+print(greet('Raj', 'klingon'))
+print(greet('John', 'elvish'))
+
