2b8817f1edf4e5944d2015bb199df4d310d3c620
shweta.borganve <shweta.borganve@gmail.com>
2024-06-04 12:40:01 +0000

    modules

--- py_basics/python_modules.py
+++ py_basics/python_modules.py
@@ -1,13 +1,14 @@
-print('hello world')
-#first-- this prints the hello world.
+print('hello world') # this prints the hello world.
 def abc():
-    print('this message is from abc function')
-#third--then executes the 4th line.
+    print('this message is from abc function') #this defines that function named abc(), when called and and print's the
+     #'this message is from abc function'
+
 def main():
-    print('this message is from main function')
-    abc()
-#second-- this line executes because("in 3rd linewe use def abc() and again in 8th linewe write abc() ")
-if __name__ == '__main__':
-    main()
-    print('now __name__ is %s' %__name__)
-#forth-- 10th line is a conditional statement. thi tells that under what condition the main() method is executed.
+    print('this message is from main function') 
+    abc() # this defines that function named main(), and print's the 'this message is from main function',calls the main() 
+    #funtion
+
+if __name__ == '__main__': #this is a conditinal statement. it checks if the script is running directly, if that is running
+    #directly, __name__ is set to '__main__' so the condition evaluates True or False 
+    main() # since the condition is True it prints "this message is from abc function."
+    print('now __name__ is %s' %__name__) # this prints the current value.
