9b2264f812ca4be9a3ff83d696e888397f7ea79f
shweta.borganve <shweta.borganve@gmail.com>
2024-06-08 17:38:40 +0000

    writing a file name

--- py_basics/py_modules/practice.txt.py
+++ py_basics/py_modules/practice.txt.py
@@ -1,8 +1,2 @@
-with open("practice.txt","r") as f:
-    data = f.read()
-
-new_data = data.replace("java","python")
-print(new_data)
-
 with open("practice.txt","w") as f:
-    f.write(new_data)
+    data = f.write("java")
