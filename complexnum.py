9d1566f96767546656cef096d323090ffcc26ddb
ShwetaBoraganve <Shweta.borganve@gmail.com>
2024-09-09 15:15:28 -0700

    compl num

---
+++ complexnum.py
@@ -0,0 +1,9 @@
+z1 = complex(2, 3)
+z2 = complex(1, -4)
+
+print("first complex number (z1):", z1)
+print("second complex number (z2):", z2)
+# performing addition:
+
+z_add = z1 + z2
+print("Addition of z1 and z2:", z_add)
