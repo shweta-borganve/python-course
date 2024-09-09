7f5396a0cb2be8f2f66c31ec5a49e2f5a8a5eba2
ShwetaBoraganve <Shweta.borganve@gmail.com>
2024-09-09 13:18:36 -0700

    integer prg

---
+++ integer.py
@@ -0,0 +1,12 @@
+a = 10
+b = 200
+
+sum_result = a+b
+substraction_result = b-a
+multiplication_result = a*b
+division_result = a//b
+
+print("Sum:", sum_result)
+print("Substraction:", substraction_result)
+print("Multiplication", multiplication_result)
+print("Division", division_result)
