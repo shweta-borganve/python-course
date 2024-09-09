fc904e65b9d7d4862f6a9ac066ce0ef101afc0f9
ShwetaBoraganve <Shweta.borganve@gmail.com>
2024-09-09 15:48:51 -0700

    list_datatype prg

---
+++ list_datatype.py
@@ -0,0 +1,17 @@
+fruits = ["apple", "banana", "cherry"]
+
+print(fruits[1])
+
+#list using append 
+fruits.append("Mango")
+print(fruits)
+print(len(fruits))
+
+# Inserting an element at a specific position using insert()
+
+fruits.insert(1, "grape")
+# insert through the list using a for a loop
+
+for fruit in fruits:
+    print(fruit)
+
