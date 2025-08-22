571b93e1aabc5122e8a56a6a937320366720cdee
shweta.borganve <shweta.borganve@gmail.com>
2025-08-22 18:06:12 +0000

    type_conversion

--- py_basics/practice_exercise/data_type.py
+++ py_basics/practice_exercise/data_type.py
@@ -3,7 +3,10 @@ age = 22 #int
 is_student = True #bool
 weight = 45.5 #float
 
-print(type(name))
-print(type(age))
-print(type(is_student))
-print(type(weight)) 
+#print(type(name))
+#print(type(age))
+#print(type(is_student))
+#print(type(weight)) 
+
+age_float = float(age)
+print(type(age_float))
