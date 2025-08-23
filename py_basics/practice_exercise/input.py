c70acda5c011f83438a4ddaf9732a42ee98d01b1
shweta.borganve <shweta.borganve@gmail.com>
2025-08-23 11:12:10 +0000

    format_string

--- py_basics/practice_exercise/input.py
+++ py_basics/practice_exercise/input.py
@@ -1,10 +1,12 @@
-boy_name=input("enter boy name")
-girl_name=input("enter girl name")
+boy_name=input("enter boy name: ")
+boy_age=int(input("enter age: "))
+girl_name=input("enter girl name: ")
+girl_age=int(input("enter girl age: "))
 
-print(boy_name)
-print(girl_name)
+age_diff = boy_age - girl_age
 
-print(boy_name + " loves " + girl_name)
+print(f"{boy_name} loves {girl_name}. age difference is {age_diff}")
+
 
 
 
