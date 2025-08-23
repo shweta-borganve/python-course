b968f6a0f3738bf88d7a97b9e972077fe3f63c50
shweta.borganve <shweta.borganve@gmail.com>
2025-08-23 10:36:00 +0000

    string concatenation

--- py_basics/practice_exercise/input.py
+++ py_basics/practice_exercise/input.py
@@ -1,2 +1,10 @@
-age = input("age :")
-print(age)
+boy_name=input("enter boy name")
+girl_name=input("enter girl name")
+
+print(boy_name)
+print(girl_name)
+
+print(boy_name + " loves " + girl_name)
+
+
+
